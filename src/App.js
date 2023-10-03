9e4165393cb6831fc93c7f1748b12f28dc44958c
Gayan Kaushalya <gaykaush@gmail.com>
2023-10-03 15:11:23 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Changing the background color of the login form to purple
+// This is the CSS code
+// .App {
+//   background-color: purple;
+//   color: white;
+//   text-align: center;
+//   font-family: Arial, Helvetica, sans-serif;
+// }
+
+
 function App() {
+  const divstyle = {
+    backgroundColor: 'purple',
+    color: 'white',
+    textAlign: 'center',
+    fontFamily: 'Arial, Helvetica, sans-serif'
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     //This is a login page
+     
+     
+     <div className="App" style={divstyle}>
+        <form>
+          <div className="form-inner">
+            <h1>Welcome!</h1>
+            <div className="form-group">
+              <label htmlFor="name">Username </label>
+              <input type="text" name="name" id="name"/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password </label>
+              <input type="password" name="password" id="password"/>
+            </div>
+            <input type="submit" value="Login"/>
+          </div>
+        </form>
+      </div>
   );
 }
 
