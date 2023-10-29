31c24807b59c16c8d8062d68f49d1d2a98dc64cf
Gayan Kaushalya <gaykaush@gmail.com>
2023-10-30 02:09:42 +0530

    Added a way to display loan requests

--- src/Components/Manager.js
+++ src/Components/Manager.js
@@ -14,6 +14,7 @@ export function Manager() {
                         <div class="Auth-form-content">
                             <h3 class="Auth-form-title" align="center">Welcome Back!</h3>                   
                             <button class="button" style="vertical-align:middle" formAction="/GenerateReport"><span>Generate a Report </span></button><br>
+                            <button class="button" style="vertical-align:middle" formAction="/ViewRequest"><span>View Loan Requests </span></button><br>
                             <button class="button" style="vertical-align:middle" formAction="/"><span>Log Out</span></button>                          
                         </div>
                     </form>
